b9659d9a23686cb819f3c0bb6b97cf4b4d3a29ac
SwiftDev <79505393@qq.com>
2017-11-04 14:52:24 +0800

    firstVersion

---
+++ JavaStudy/JavaStudy/src/FirstPackage/FirstClass.java
@@ -0,0 +1,15 @@
+package FirstPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SecondClass secondClass = new SecondClass();
+		System.out.print(secondClass.Name);
+	}
+
+}
+
+class SecondClass{
+	public String Name = "SecondClass";
+}
